da9253828b146137f606ab4f9a47ba8f4a5e25e8
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2020-11-04 16:29:36 +0100

    Add files via upload

---
+++ FrontEnd/Factory.cs
@@ -0,0 +1,21 @@
+﻿using BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrontEnd
+{
+    public class Factory
+    {
+        public static ILogin NewLogin() => new Login();
+
+        //public static ILogin NewLogin()
+        //{
+        //    return new Login();
+        //}
+
+        public static ILoginDetails NewLoginDetails() => new LoginDetails();
+
+        public static IFeature1 NewFEature1() => new Feature1();
+    }
+}
